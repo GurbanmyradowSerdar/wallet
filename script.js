daf581b105fd4369fa64074f6620301f54c2fec2
Serdar <115923465+GurbanmyradowSerdar@users.noreply.github.com>
2023-10-01 23:39:37 +0500

    did script of in and out card just still some work here

--- script.js
+++ script.js
@@ -1,209 +1,25 @@
 class In {
   text;
   money;
-  constructor(text, money) {
+  date;
+  constructor(text, money, date) {
     this.text = text;
     this.money = money;
+    this.date = date;
   }
 }
 let inArr = [];
 let outArr = [];
-let result = 0;
-let isFirstVisit = false;
-
 class Out {
   text;
   money;
-  constructor(text, money) {
+  date;
+  constructor(text, money, date) {
     this.text = text;
     this.money = money;
+    this.date = date;
   }
 }
-
-// Show input block ///////////////////////////////////////////////////////
-// function showInput(which) {
-//   let box = document.getElementById("box");
-//   box.style.display = "none";
-
-//   let cont = document.createElement("div"),
-//     text = document.createElement("input"),
-//     money = document.createElement("input"),
-//     chooseCont = document.createElement("div"),
-//     ok = document.createElement("button"),
-//     cancel = document.createElement("button");
-
-//   let body = document.body;
-
-//   cont.id = "input";
-//   text.id = "input__text";
-//   money.id = "input__money";
-//   chooseCont.id = "input__choose";
-//   cancel.id = "input__cancel";
-//   ok.id = "input__ok";
-
-//   money.placeholder = "Ex 30...";
-//   ok.innerHTML = "OK";
-//   cancel.innerHTML = "Cancel";
-
-//   append(cont, text);
-//   append(cont, money);
-//   append(chooseCont, ok);
-//   append(chooseCont, cancel);
-//   append(cont, chooseCont);
-
-//   append(body, cont);
-
-//   cancel.onclick = () => {
-//     body.removeChild(cont);
-//     box.style.display = "flex";
-//   };
-
-//   if (which) {
-//     text.placeholder = "Ex website...";
-
-//     ok.onclick = function okClick() {
-//       let a = text.value.trim();
-//       let b = money.value.trim();
-
-//       if (a.length === 0 || isNaN(b) || b.length === 0) {
-//       } else {
-//         ok.style.display = "flex";
-//         inArr.push(new In(a, b));
-
-//         result += Number(b);
-//         body.removeChild(cont);
-//         box.style.display = "flex";
-//       }
-//     };
-//   } else {
-//     text.placeholder = "Ex food...";
-
-//     ok.onclick = function okClick() {
-//       let a = text.value;
-//       let b = money.value;
-
-//       if (a.length === 0 || isNaN(b) || b.length === 0) {
-//       } else {
-//         if (b < result) {
-//           ok.style.display = "flex";
-//           outArr.push(new Out(a, b));
-
-//           result -= Number(b);
-
-//           body.removeChild(cont);
-//           box.style.display = "flex";
-//         } else {
-//           alert("You don't have enough money for do this");
-//         }
-//       }
-//     };
-//   }
-// }
-
-// show result ///////////////////////////////////////////////////////
-// function showRes() {
-//   let box = document.getElementById("box");
-//   box.style.display = "none";
-
-//   let cont = document.createElement("div"),
-//     res = document.createElement("h1"),
-//     desIn = document.createElement("h3"),
-//     desOut = document.createElement("h3"),
-//     textIn = document.createElement("div"),
-//     textOut = document.createElement("div"),
-//     chooseCont = document.createElement("div"),
-//     ok = document.createElement("button"),
-//     clear = document.createElement("button");
-
-//   let body = document.body;
-
-//   textIn.className = "result__box";
-//   textOut.className = "result__box";
-//   cont.id = "show-box";
-//   res.id = "show__res";
-//   chooseCont.id = "input__choose";
-//   clear.id = "input__cancel";
-//   ok.id = "input__ok";
-
-//   res.innerHTML = `Result = ${result}$`;
-//   ok.innerHTML = "OK";
-//   clear.innerHTML = "Clear";
-//   let answer = 0;
-//   inArr.forEach((item, i) => (answer += Number(item.money)));
-//   desIn.innerHTML = `Income : ${answer}$`;
-//   answer = 0;
-//   outArr.forEach((item, i) => (answer += Number(item.money)));
-//   desOut.innerHTML = `Expense : ${answer}$`;
-
-//   inArr.forEach((elem, i) => {
-//     let t = document.createElement("p");
-//     let t1 = document.createElement("p");
-//     t.innerHTML = `Description : ${elem.text}`;
-//     t1.innerHTML = `Money : ${elem.money}$`;
-//     append(textIn, t);
-//     append(textIn, t1);
-//   });
-
-//   outArr.forEach((elem, i) => {
-//     let t = document.createElement("p");
-//     let t1 = document.createElement("p");
-//     t.innerHTML = `Description : ${elem.text}`;
-//     t1.innerHTML = `Money : ${elem.money}$`;
-//     append(textOut, t);
-//     append(textOut, t1);
-//   });
-
-//   append(cont, res);
-//   append(cont, desIn);
-//   append(cont, textIn);
-//   append(cont, desOut);
-//   append(cont, textOut);
-//   append(chooseCont, ok);
-//   append(chooseCont, clear);
-//   append(cont, chooseCont);
-
-//   append(body, cont);
-
-//   ok.onclick = () => {
-//     body.removeChild(cont);
-//     box.style.display = "flex";
-//   };
-
-//   clear.onclick = () => {
-//     desIn.innerHTML = `Income : 0$`;
-//     desOut.innerHTML = `Expense : 0$`;
-//     result = 0;
-//     window.localStorage.clear();
-//     inArr = [];
-//     outArr = [];
-//     res.innerHTML = `Result = ${result}$`;
-//     textIn.innerHTML = "";
-//     textOut.innerHTML = "";
-//   };
-// }
-
-// let income = document.getElementById("income");
-// // Onclick income ///////////////////////////////////////////////////////
-// income.onclick = () => {
-//   showInput(true);
-// };
-
-// let expense = document.getElementById("expense");
-// // Onclick expense ///////////////////////////////////////////////////////
-// expense.onclick = () => {
-//   showInput(false);
-// };
-
-// let show = document.getElementById("show");
-// // Onclick show ///////////////////////////////////////////////////////
-// show.onclick = () => {
-//   showRes();
-// };
-
-// append ///////////////////////////////////////////////////////
-// function append(parent, elem) {
-//   parent.appendChild(elem);
-// }
 
 // Set values in localStorage ///////////////////////////////////////////////////////
 function save() {
@@ -251,6 +67,9 @@ function load() {
     setPriceBoardsDetails();
     // ! user profile
     setUserProfileCardDetails();
+    // ! in and out cards
+    setInCardDetails();
+    setOutCardDetails();
   } else {
     confirmButton.addEventListener("click", function () {
       if (registerValidator()) {
@@ -442,9 +261,92 @@ function setUserProfileCardDetails() {
 
   setIncomeLimit;
   setExpenseLimit;
-  deleteAll;
+  deleteAll.onclick = function (e) {};
 }
 
+// ! set in cards details
+function setInCardDetails() {
+  let incomeAmount = document.getElementById("pricecard-income-amount"),
+    incomeDesc = document.getElementById("pricecard-income-desc"),
+    incomeCancel = document.getElementById("pricecard-in-cancel"),
+    incomeAdd = document.getElementById("pricecard-in-add");
+
+  // ! validation add
+  incomeAdd.onclick = function (e) {
+    if (
+      incomeAmount.value.length > 0 &&
+      !isNaN(incomeAmount.value) &&
+      incomeDesc.value.length > 0
+    ) {
+      incomeAmount.parentElement.children.item(0).style.color = "white";
+      incomeDesc.parentElement.children.item(0).style.color = "white";
+
+      inArr.push(
+        new In(
+          incomeDesc.value,
+          incomeAmount.value,
+          `${new Date().getDate()}/${
+            new Date().getMonth() + 1
+          }/${new Date().getFullYear()}`
+        )
+      );
+      incomeAmount.value = "";
+      incomeDesc.value = "";
+    } else {
+      incomeAmount.parentElement.children.item(0).style.color = "red";
+      incomeDesc.parentElement.children.item(0).style.color = "red";
+    }
+  };
+  // ! cancel handling
+  incomeCancel.onclick = function (e) {
+    incomeAmount.value = "";
+    incomeDesc.value = "";
+    incomeAmount.parentElement.children.item(0).style.color = "white";
+    incomeDesc.parentElement.children.item(0).style.color = "white";
+  };
+}
+
+// ! set out cards details
+function setOutCardDetails() {
+  let expenseAmount = document.getElementById("pricecard-expense-amount"),
+    expenseDesc = document.getElementById("pricecard-expense-desc"),
+    expenseCancel = document.getElementById("pricecard-out-cancel"),
+    expenseAdd = document.getElementById("pricecard-out-add");
+
+  // ! validation add
+  expenseAdd.onclick = function (e) {
+    if (
+      expenseAmount.value.length > 0 &&
+      !isNaN(expenseAmount.value) &&
+      expenseDesc.value.length > 0
+    ) {
+      expenseAmount.parentElement.children.item(0).style.color = "white";
+      expenseDesc.parentElement.children.item(0).style.color = "white";
+
+      outArr.push(
+        new Out(
+          expenseDesc.value,
+          expenseAmount.value,
+          `${new Date().getDate()}/${
+            new Date().getMonth() + 1
+          }/${new Date().getFullYear()}`
+        )
+      );
+      expenseAmount.value = "";
+      expenseDesc.value = "";
+    } else {
+      expenseAmount.parentElement.children.item(0).style.color = "red";
+      expenseDesc.parentElement.children.item(0).style.color = "red";
+    }
+  };
+  // ! cancel handling
+  expenseCancel.onclick = function (e) {
+    expenseAmount.value = "";
+    expenseDesc.value = "";
+    expenseAmount.parentElement.children.item(0).style.color = "white";
+    expenseDesc.parentElement.children.item(0).style.color = "white";
+  };
+}
 // !isNull cheker
 function isNull(value) {
   if (value === null) return true;
